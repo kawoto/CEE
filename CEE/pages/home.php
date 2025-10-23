75e3be1a06f6b140f6daa64934a2a57c7b98599e
Admin <bogalekawoto@gmail.com>
2025-10-23 08:44:53 -0700

    first commit

---
+++ CEE/pages/home.php
@@ -0,0 +1,20 @@
+
+<link rel="stylesheet" type="text/css" href="css/mycss.css">
+<div class="app-main__outer" style="background-image: url(login-ui/images/on.jpg)"; style="background-size: contain; position: relative; " >
+    <img src="login-ui/images/testbg.png" style="width: 100%; height:auto;">
+<div class="app-main__outer">
+        <div class="app-main__inner">
+            
+               
+                    <div class="page-title-heading">
+                        
+                    </div>
+                       
+            </div>
+        </div>
+            <div class="col-md-12">
+                <div class="main-card mb-3 card">
+<div class="app-main__outer">
+    <div id="refreshData">
+            
+    </div>
